2b47519e52ec532bc9d987e9d67a632c9e747447
Gajy <Gaji1@yandex.rugit config user.emailgit config --global user.email Gaji1@yandex.ru>
2023-10-15 12:17:05 +0300

    Completed

---
+++ HomeWork_1/HomeWork_1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace HomeWork_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var grand_father = new AdultFamilyMember() { Mother = null, Name = "Bob", WifeName = "Hana", Sex = Gender.Male };
+            var father = new AdultFamilyMember() { Father = grand_father, Name = "Папа",WifeName = "Мама", Sex = Gender.Male };
+            var mother = new AdultFamilyMember() { Mother = null, Name = "Мама", Sex = Gender.Female };
+            var son = new FamilyMember() { Father = father, Mother = mother, Name = "Сын", WifeName = "Не женат", Sex = Gender.Male };
+            var son2 = new FamilyMember() { Father = father, Mother = mother, Name = "Сын", WifeName = "Не женат", Sex = Gender.Male };
+            grand_father.Children = new FamilyMember[] { father };
+            mother.Children = new FamilyMember[] { son,son2 };
+            father.Children = new FamilyMember[] { son,son2 };
+            grand_father.Print(1);
+            Console.ReadKey();
+        } 
+    }
+}
